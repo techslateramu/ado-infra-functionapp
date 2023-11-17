977aec4bab9494dff49f63163dd417fd60aee246
Ramu Gurrapu <ramu@techslatelimited.com>
2023-11-17 22:49:54 +0000

    added storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,32 @@
 data "azurerm_client_config" "current" {}
 
 module "resource_group" {
-  source                                      = "../modules/resourcegroup"
-  environment                                 = var.environment
-  location                                    = var.location
-  main_project                                = var.main_project
-  sub_project                                 = var.sub_project
-  tags                                        = merge(var.tags, var.specific_tags)
+  source                   = "github.com/techslateramu/terraform-modules//azure/resourcegroup"
+  environment              = var.environment
+  location                 = var.location
+  main_project             = var.main_project
+  sub_project              = var.sub_project
+  tags                     = merge(var.tags, var.specific_tags)
 }
 
-module "container_instance" {
-  source                                      = "../modules/containerinstance"
-  environment                                 = var.environment
-  location                                    = var.location
-  main_project                                = var.main_project
-  sub_project                                 = var.sub_project
-  tags                                        = merge(var.tags, var.specific_tags)
-  resource_group_name                         = module.resource_group.name
+resource "azurerm_storage_account" "storage" {
+  source                   = "github.com/techslateramu/terraform-modules//azure/storageaccount"
+  name                     = local.name
+  resource_group_name      = module.resource_group.name
+  location                 = var.location
+  main_project             = var.main_project
+  sub_project              = var.sub_project
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  tags                     = merge(var.tags, var.specific_tags)
 }
 
+# module "key_vault" {    
+#   source                                      = "../modules/keyvault"
+#   depends_on                                  = [ module.resource_group ]
+#   kv_name                                                                                                                                                                                = var.kv_name
+#   rg_name                                                                                                                                             = var.rg_name
+#   location                                    = var.location  
+#   tenant_id                                   = data.azurerm_client_config.current.tenant_id
+#   object_id                                   = data.azurerm_client_config.current.object_id
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,51 +1,3 @@
-variable "rg_name" {
-  description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."  
-  default = "techslate-ade-rg"
-}
-
-variable "location" {
-    description =  "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."    
-    default = "northeurope"  
-}
-
-variable "tags" {
-    description =  "(Optional) A mapping of tags which should be assigned to the Resource Group."
-    default = {  
-      "source" = "terraform"
-      "env"    = "dev"
-      "cost"   = "163"
-      "dept"   = "finance"
-    }
-}
-
-
-variable "kv_name" {
-  description = ""
-  default = "techslate-ade-kv001"
-}
-
-variable "st_name" {
-  description = "(Required) Specifies the name of the storage account. Changing this forces a new resource to be created. This must be unique across the entire Azure service, not just within the resource group."
-  default = "techslatestorage001"
-}
-
-variable "asp_name" {
-  description = ""
-  default = "techslate-asp-001"
-}
-
-variable "fun_name" {
-  description = ""
-  default = "techslate-function-001"
-}
-
-variable "storage_account_access_key" {
-    default = "techslate-function-001"
-}
-
-
-=====
-
 variable "environment" { default = "dev" }              # should be coming from env folder for ex: tst.tfvars
 variable "tags" {
     default = {
@@ -59,12 +11,11 @@ variable "ARM_TENANT_ID"        {}
 variable "ARM_CLIENT_ID"        {}
 variable "ARM_CLIENT_SECRET"    {}
 
-variable "specific_tags"        { default = {} }
+variable "specific_tags"            { default = {} }
+variable "main_project"             { default = "func" }   # Set this VARIABLE - this will form  the name of RESOURCES
+variable "sub_project"              { default = "01" }
+variable "location"                 { default = "uksouth" }                             # https://github.com/claranet/terraform-azurerm-regions/blob/master/regions.tf
+variable "account_tier"             { default = "Standard" }
+variable "account_replication_type" { default = "GRS" }
 
-variable "main_project"         { default = "" }   # Set this VARIABLE - this will form  the name of RESOURCES
-
-variable "sub_project"          { default = "01" }
-variable "location"             { default = "uksouth" }                             # https://github.com/claranet/terraform-azurerm-regions/blob/master/regions.tf
-variable "container_image"      { default = "linuxramu/calc:2.5" }
-
-variable "dns_name_label"       { default = "" }  # Set this VARIABLE - this will be URL of application, should be unique
+# variable "storage_account_access_key"   { default = "techslate-function-001" }
